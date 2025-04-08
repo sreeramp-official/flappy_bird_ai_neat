402e8086efc299e04fbcbcaced5718d3995c0214
Sreeram P <psreeram3015@proton.me>
2025-04-08 19:25:21 +0530

    Created functions to move and draw the bird

--- flappy_game.py
+++ flappy_game.py
@@ -41,3 +41,62 @@ class Bird:
         self.vel = -10.5
         self.tick_count = 0
         self.height = self.y
+
+    def move(self):
+        self.tick_count += 1
+        d = self.vel * self.tick_count + 1.5 * self.tick_count**2
+
+        if d >= 16:
+            d = 16
+
+        elif d < 0:
+            d -= 2
+
+        self.y = self.y + d
+
+        if d < 0 or self.y < self.height + 50:
+            if self.tilt < self.MAX_ROTATION:
+                self.tilt = self.MAX_ROTATION
+        else:
+            if self.tilt > -90:
+                self.tilt -= self.ROT_VEL
+
+    def draw(self, win):
+        self.img_count += 1
+
+        # For animation of bird, loop through three images
+        if self.img_count <= self.ANIMATION_TIME:
+            self.img = self.IMGS[0]
+        elif self.img_count <= self.ANIMATION_TIME * 2:
+            self.img = self.IMGS[1]
+        elif self.img_count <= self.ANIMATION_TIME * 3:
+            self.img = self.IMGS[2]
+        elif self.img_count <= self.ANIMATION_TIME * 4:
+            self.img = self.IMGS[1]
+        elif self.img_count == self.ANIMATION_TIME * 4 + 1:
+            self.img = self.IMGS[0]
+            self.img_count = 0
+
+        # so when bird is nose diving it isn't flapping
+        if self.tilt <= -80:
+            self.img = self.IMGS[1]
+            self.img_count = self.ANIMATION_TIME * 2
+
+        rotated_image = pygame.transform.rotate(self.img, self.tilt)
+
+        new_rect = rotated_image.get_rect(
+            center=self.img.get_rect(topleft=(self.x, self.y)).center
+        )
+
+        win.blit(rotated_image, new_rect.topleft)
+
+    def get_mask(self):
+        return pygame.mask.from_surface(self.img)
+
+
+# def draw_window(win, bird):
+#     pass
+
+
+# def main():
+#     pass
