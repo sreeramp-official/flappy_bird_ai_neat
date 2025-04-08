50f9e0c4ded16bc09e898f8b3378417a1cba8121
Sreeram P <psreeram3015@proton.me>
2025-04-08 19:11:17 +0530

    Created a function jump in bird class

--- flappy_game.py
+++ flappy_game.py
@@ -19,3 +19,25 @@ BASE_IMG = pygame.transform.scale2x(
     pygame.image.load(os.path.joins("assets", "base.png"))
 )
 BG_IMG = pygame.transform.scale2x(pygame.image.load(os.path.joins("assets", "bg.png")))
+
+
+class Bird:
+    IMGS = BIRD_IMGS
+    MAX_ROTATION = 25
+    ROT_VEL = 20
+    ANIMATION_TIME = 5
+
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+        self.tilt = 0
+        self.tick_count = 0
+        self.vel = 0
+        self.height = self.y
+        self.img_count = 0
+        self.img = self.IMGS[0]
+
+    def jump(self):
+        self.vel = -10.5
+        self.tick_count = 0
+        self.height = self.y
